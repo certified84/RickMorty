f5ac223141935039e57bdadb478c1d67df80427f
Samson Achiaga <sirhumble84@gmail.com>
2022-09-24 12:05:57 +0100

    Update UI implementation
    - Switched to XML from compose

--- app/src/main/java/com/certified/rickmorty/MainActivity.kt
+++ app/src/main/java/com/certified/rickmorty/MainActivity.kt
@@ -1,98 +1,29 @@
 package com.certified.rickmorty
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.certified.rickmorty.ui.CharacterGrid
+import androidx.appcompat.app.AppCompatActivity
+import com.certified.rickmorty.databinding.ActivityMainBinding
 import com.certified.rickmorty.ui.RickMortyViewModel
-import com.certified.rickmorty.ui.theme.RickMortyTheme
-import com.certified.rickmorty.ui.theme.SpaceGrotesk
-import com.intuit.sdp.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private val viewModel: RickMortyViewModel by viewModels()
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+//    private val viewModel: RickMortyViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContent {
-            RickMortyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(
-                                top = dimensionResource(id = R.dimen._16sdp),
-                                start = dimensionResource(id = R.dimen._16sdp),
-                                end = dimensionResource(id = R.dimen._16sdp)
-                            ),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Log.d(
-                            "TAG",
-                            "onCreate: ${viewModel.pagingDataFlow.collectAsLazyPagingItems().itemCount}"
-                        )
-
-                        Spacer(modifier = Modifier.padding(10.dp))
-
-                        Text(
-                            text = "Rick & Morty - Compose",
-                            fontSize = 24.sp,
-                            fontFamily = SpaceGrotesk,
-                            fontWeight = FontWeight.Bold,
-//                    color = Color.White,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        )
-
-                        Spacer(modifier = Modifier.padding(16.dp))
-
-                        CharacterGrid(items = viewModel.pagingDataFlow.collectAsLazyPagingItems())
-
-                    }
-                }
-            }
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RickMortyTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/certified/rickmorty/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.certified.rickmorty.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Primary = Color(0xF7C49D)
